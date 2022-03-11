46e81437b6e90fe9c5ed347e8b483109391600a2
nemes05 <nemes.barnabas020513@gmail.com>
2022-03-11 15:26:05 +0100

    added excel

--- convert2.py
+++ convert2.py
@@ -1,3 +1,4 @@
+from typing import Type
 import win32com.client
 import os
 import glob
@@ -8,12 +9,16 @@ import glob
 powPoints = []
 #stores the names of the Word documents
 words = []
+#stores the names of the Excel documents
+excels = []
 
-#put the names of the Powerpoint and Word documents to the correct array
+#put the names of the Powerpoint, Word and Excel documents to the correct array
 for x in glob.glob("*.pptx"):
     powPoints.append(x)
 for x in glob.glob("*.docx"):
     words.append(x)
+for x in glob.glob("*.xls"):
+    excels.append(x)
 
 #converts the Powerpoints to pdf
 for x in powPoints:
@@ -32,10 +37,22 @@ for x in words:
     in_file = os.path.realpath(x)
     x = x.replace('.docx','')
     out_file = os.getcwd() + "\\" + x + ".pdf"
-    print(out_file)
     word = win32com.client.gencache.EnsureDispatch('Word.Application')
     word.Visible = True
     pdf = word.Documents.Open(in_file)
     pdf.SaveAs(out_file, FileFormat = 17)
     pdf.Close()
     word.Quit()
+
+#converts the Excel documents to pdf 
+for x in excels:
+    in_file = os.path.realpath(x)
+    x = x.replace('.xls','')
+    out_file = os.getcwd() + "\\" + x + ".pdf"
+    excel = win32com.client.gencache.EnsureDispatch('Excel.Application')
+    excel.Visible = True
+    pdf = excel.Workbooks.Open(in_file)
+    #pdf.SaveAs(out_file, FileFormat = 17)
+    pdf.ExportAsFixedFormat(0,out_file)
+    pdf.Close()
+    excel.Quit()
